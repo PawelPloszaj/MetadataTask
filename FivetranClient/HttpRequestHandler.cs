a243025a8d06058a21c92530572f138b90a7415f
PawelPloszaj <41387367+PawelPloszaj@users.noreply.github.com>
2025-08-11 23:19:53 +0200

    fix: correct semaphore initialization and _GetAsync retry logic

--- FivetranClient/HttpRequestHandler.cs
+++ FivetranClient/HttpRequestHandler.cs
@@ -23,54 +23,72 @@ public class HttpRequestHandler
         this._client = client;
         if (maxConcurrentRequests > 0)
         {
-            this._semaphore = new SemaphoreSlim(0, maxConcurrentRequests);
+            this._semaphore = new SemaphoreSlim(maxConcurrentRequests, maxConcurrentRequests);  // Fixed semaphore initialization (Semafora startowała z 0 wolnymi miejscami, więc wszystkie wątki będą zablokowane na WaitAsync()
         }
     }
 
     public async Task<HttpResponseMessage> GetAsync(string url, CancellationToken cancellationToken)
     {
-        return _responseCache.GetOrAdd(
+        return await _responseCache.GetOrAddAsync(
             url,
-            () => this._GetAsync(url, cancellationToken).Result,
+            () => this._GetAsync(url, cancellationToken),
             TimeSpan.FromMinutes(60));
     }
 
     private async Task<HttpResponseMessage> _GetAsync(string url, CancellationToken cancellationToken)
     {
-        if (this._semaphore is not null)
+        bool shouldRetry;
+
+        do
         {
-            await this._semaphore.WaitAsync(cancellationToken);
-        }
+            shouldRetry = false;
 
-        TimeSpan timeToWait;
-        lock (this._lock)
-        {
-            timeToWait = this._retryAfterTime - DateTime.UtcNow;
-        }
-
-        if (timeToWait > TimeSpan.Zero)
-        {
-            await Task.Delay(timeToWait, cancellationToken);
-        }
-
-        cancellationToken.ThrowIfCancellationRequested();
-
-        var response = await this._client.GetAsync(new Uri(url, UriKind.Relative), cancellationToken);
-        response.EnsureSuccessStatusCode();
-        if (response.StatusCode is HttpStatusCode.TooManyRequests)
-        {
-            var retryAfter = response.Headers.RetryAfter?.Delta ?? TimeSpan.FromSeconds(60);
-
-            lock (this._lock)
+            if (this._semaphore is not null)
             {
-                this._retryAfterTime = DateTime.UtcNow.Add(retryAfter);
+                await this._semaphore.WaitAsync(cancellationToken);
             }
 
-            // new request will wait for the specified time before retrying
-            return await this._GetAsync(url, cancellationToken);
-        }
+            try
+            {
+                TimeSpan timeToWait;
+                lock (this._lock)
+                {
+                    timeToWait = this._retryAfterTime - DateTime.UtcNow;
+                }
 
-        this._semaphore?.Release();
-        return response;
+                if (timeToWait > TimeSpan.Zero)
+                {
+                    await Task.Delay(timeToWait, cancellationToken);
+                }
+
+                cancellationToken.ThrowIfCancellationRequested();
+
+                var response = await this._client.GetAsync(new Uri(url, UriKind.Relative), cancellationToken);
+
+                if (response.StatusCode is HttpStatusCode.TooManyRequests)
+                {
+                    var retryAfter = response.Headers.RetryAfter?.Delta ?? TimeSpan.FromSeconds(60);
+
+                    lock (this._lock)
+                    {
+                        this._retryAfterTime = DateTime.UtcNow.Add(retryAfter);
+                    }
+
+                    // new request will wait for the specified time before retrying
+                    shouldRetry = true;
+                    continue;
+                }
+
+                response.EnsureSuccessStatusCode();
+                return response;
+            }
+            finally
+            {
+                this._semaphore?.Release();
+            }
+        } while (shouldRetry);
+
+        throw new InvalidOperationException("Failed to get HTTP response");
     }
+
 }
